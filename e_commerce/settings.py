4ae3378ced1c938a01f8ff99a06a77e601d8dd44
Shopon-Hossen <extremeediting572@gmail.com>
2025-04-20 20:51:54 +0600

    update settings.py

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -23,22 +23,28 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# SECURITY WARNING: set only hosts that you trust (you're own hosts only)
 ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    # Third party packages
     "channels",
     "corsheaders",
     "rest_framework",
     "daphne",
+
+    # Django built-in
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
     "account",
     "shop",
     "search",
@@ -82,14 +88,6 @@ ASGI_APPLICATION = 'e_commerce.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "e_commerce.sqlite3"
-#     }
-# }
-
 
 DATABASES = {
     'default': {
@@ -148,7 +146,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Custom
 AUTH_USER_MODEL = 'account.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -168,7 +165,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20
 }
 
-
+ # Must change in production
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(weeks=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(weeks=1),
@@ -181,16 +178,16 @@ DEFAULT_SHOP_LOGO_IMAGE = "image/default_shop_logo.jpg"
 DEFAULT_SHOP_BANNER_IMAGE = "image/default_shop_banner.jpg"
 DEFAULT_PRODUCT_IMAGE = "image/default_product.jpg"
 
+# 5 MB
 MAX_UPLOAD_SIZE = 5 * 1024 * 1024
 
 
-# Redis backend for real-time WebSocket communication
+# SECURITY WARNING: Use Redis in production
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",  # Use Redis in production
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
     },
 }
-
 
 # Example redis
 # CHANNEL_LAYERS = {
@@ -201,3 +198,14 @@ CHANNEL_LAYERS = {
 #         },
 #     },
 # }
+
+
+## Security (Only for real-world production use)
+## Automatically redirect all HTTP traffic to HTTPS. http://yourdomain.com > https://yourdomain.com
+# SECURE_SSL_REDIRECT = True
+
+## This ensures that the CSRF token is only sent over secure (HTTPS) connections
+# CSRF_COOKIE_SECURE = True
+
+## It makes sure that session cookies (which Django uses to track a user across requests) are only sent over secure connections (HTTPS)
+# SESSION_COOKIE_SECURE = True
