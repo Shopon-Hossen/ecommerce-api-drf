d6c337e0f789d138c22229951e091dc542256bf6
Shopon-Hossen <extremeediting572@gmail.com>
2025-04-29 21:25:54 +0600

    added postman collection file

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from account.models import User
 
 
-
 class ChatMessage(models.Model):
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
